5eb1bcc7bcc4e35ec9e85cd92cb9cd0618379c22
Dawnhill2k <149022001+Dawnhill2k@users.noreply.github.com>
2026-04-29 20:15:49 +0100

    Add files via upload

---
+++ api/save-typing.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+
+  if (req.method === "POST") {
+
+    console.log("Typing:", req.body);
+
+    return res.status(200).json({
+      success: true
+    });
+  }
+
+  return res.status(405).json({
+    error: "Method not allowed"
+  });
+
+}
